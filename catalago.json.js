7db07995edacb94a3cc3fd4ea19afe16a039a1c6
Lydia4243 <112407983+Lydia4243@users.noreply.github.com>
2022-10-18 00:09:54 -0500

    primer commit
    catalogo de tuupers

---
+++ catalago.json.js
@@ -0,0 +1,9 @@
+let stockProductos = [
+    
+    
+    {"id": 1, "nombre": "Cubeta Ballena", "tipo": "Cubeta", "cantidad": 1, "desc": "Color Azul", "precio": 1000, "capacidad": "8", "img": "https://res.cloudinary.com/dkuurazwg/image/upload/v1665862516/lyly/Cubeta_Ballena_c429ui.jpg"},
+    {"id": 2, "nombre": "Emplayer Amarillo", "tipo": "Tuppers", "cantidad": 1, "desc": "Amarillo", "precio": 1000, "capacidad": "10", "img": "https://res.cloudinary.com/dkuurazwg/image/upload/v1665862422/lyly/Emplayer_Amarillo_gemlih.jpg"},
+    {"id": 3, "nombre": "Gelatina Chico", "tipo": "Rueda", "cantidad": 1, "desc": "Roja", "precio": 1000, "capacidad": "1", "img": "https://res.cloudinary.com/dkuurazwg/image/upload/v1665862273/lyly/Jarra_rqlap0.jpg"},
+    {"id": 4, "nombre": "Jarra", "tipo": "Clasica", "cantidad": 1, "desc": "Roja", "precio": 1000, "capacidad": 3.5, "img": "https://res.cloudinary.com/dkuurazwg/image/upload/v1665862273/lyly/Jarra_rqlap0.jpg"}
+  
+]
